58eae0a118dd935859b155bb8e852bd9f240f701
cherelemma <chere.lemma@aastu.edu.et>
2023-06-02 23:02:59 +0300

    add calculator book

---
+++ calculator.rb
@@ -0,0 +1,22 @@
+# simple calculator class
+class Calculator
+  def add(num1, num2)
+    num1 + num2
+  end
+
+  def subtract(num1, num2)
+    num1 - num2
+  end
+
+  def multiplnum2(num1, num2)
+    num1 * num2
+  end
+
+  def divide(num1, num2)
+    num1 / num2
+  end
+
+  def modulo(num1, num2)
+    num1 % num2
+  end
+end
