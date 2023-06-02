06ae6e7150aec348a9cab757b9cb152c5a34402e
cherelemma <chere.lemma@aastu.edu.et>
2023-06-02 17:46:21 +0300

    add factorial instance method

--- solver.rb
+++ solver.rb
@@ -1,4 +1,10 @@
 # solver class
 class Solver
+  def factorial(num)
+    raise 'ArgumentError: n must be >= 0' if num.negative?
 
+    return 1 if num.zero?
+
+    num * factorial(num - 1)
+  end
 end
