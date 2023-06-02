f993d4c62813065a2be8b3cee8d3f7dc7f9b1479
Odongo006 <www.henryodongo006@gmail.com>
2023-06-02 17:38:17 +0300

    Add fizzbuzz

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,3 +33,32 @@ describe 'Solver#reverse' do
     @solver.reverse('chere').should == 'erehc'
   end
 end
+
+describe 'Solver#fizzbuzz' do
+  context 'when passing number divisible by 3' do
+    it "should return 'fizz' when given 3" do
+      @solver.fizzbuzz(3).should == 'fizz'
+    end
+  end
+
+  context 'when passing number divisible by 5' do
+    it "should return 'buzz' when given 5" do
+      @solver.fizzbuzz(5).should == 'buzz'
+    end
+  end
+
+  context 'when passing number divisible by both 3 and 5' do
+    it "should return 'fizzbuzz' when given 15" do
+      @solver.fizzbuzz(15).should == 'fizzbuzz'
+    end
+    it "should return 'fizzbuzz' when given 30" do
+      @solver.fizzbuzz(30).should == 'fizzbuzz'
+    end
+  end
+
+  context 'when passing number not divisible by either 3 or 5' do
+    it "should return '17' when given 17" do
+      @solver.fizzbuzz(17).should == '17'
+    end
+  end
+end  
